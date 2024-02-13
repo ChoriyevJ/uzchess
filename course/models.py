6638feefa92c8ca4ab44a4c34db0c4f047dd39ee
ChoriyevJ <97903948+ChoriyevJ@users.noreply.github.com>
2024-02-13 18:21:43 +0500

    feat: commit with first changes

---
+++ course/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+from utils.models import BaseModel
+
+
+class Course(BaseModel):
+    pass
+
+
+class Lesson(BaseModel):
+    pass
+
+
+class Comment(BaseModel):
+    pass
+
+
+
+
+
+
+
+
+
+
---
+++ library/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from utils.models import BaseModel
+
+
+class Book(BaseModel):
+    pass
+
+
+class Post(BaseModel):
+    pass
+
+
